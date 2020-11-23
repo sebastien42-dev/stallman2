e61331cedbe126b0a800cbdbdfd4434029a4e177
seb <s.delclos@skara.ovh>
2020-11-23 13:38:04 +0100

    ajout des matieres dans l index des classes

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use App\Entity\Classe;
 use App\Form\ClasseType;
 use App\Repository\ClasseRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin/classe")
