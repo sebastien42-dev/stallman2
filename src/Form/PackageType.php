7c9559c2ace050ff9041138fd74704b1a603db3d
sebastien42-dev <sebmiren42@gmail.com>
2021-02-02 14:52:17 +0100

    fix visuel crud package

--- src/Form/PackageType.php
+++ src/Form/PackageType.php
@@ -12,8 +12,24 @@ class PackageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('packageName')
-            ->add('value')
+            ->add('packageName',NULL,[
+                'attr' => [
+                    'class'=> 'form-control border border-dark mb-2'
+                ],
+                'label' => "Libelle du forfait",
+                'label_attr' => [
+                'class' => 'text-dark font-weight-bold'
+                ]
+            ])
+            ->add('value',NULL,[
+                'attr' => [
+                    'class'=> 'form-control border border-dark mb-2'
+                ],
+                'label' => "Montant unitaire",
+                'label_attr' => [
+                'class' => 'text-dark font-weight-bold'
+                ]
+            ])
         ;
     }
 
