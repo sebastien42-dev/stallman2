c277d132e96562f4b7274d461009ff9f8d25ecb8
sebastien42-dev <sebmiren42@gmail.com>
2021-02-02 11:36:25 +0100

    ajout de package et commentaire de migration

--- migrations/Version20201123085021.php
+++ migrations/Version20201123085021.php
@@ -20,9 +20,9 @@ final class Version20201123085021 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE civilite (id INT AUTO_INCREMENT NOT NULL, libelle_civilite VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64957889920 FOREIGN KEY (fonction_id) REFERENCES fonction (id)');
-        $this->addSql('CREATE INDEX IDX_8D93D64957889920 ON user (fonction_id)');
+        //$this->addSql('CREATE TABLE civilite (id INT AUTO_INCREMENT NOT NULL, libelle_civilite VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        //$this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64957889920 FOREIGN KEY (fonction_id) REFERENCES fonction (id)');
+        //$this->addSql('CREATE INDEX IDX_8D93D64957889920 ON user (fonction_id)');
     }
 
     public function down(Schema $schema) : void
