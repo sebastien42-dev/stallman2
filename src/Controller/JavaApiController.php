5b1fbae47da301832cf75a4d8c87f3d34ffd2b9d
seb <s.delclos@skara.ovh>
2021-04-27 09:09:50 +0200

    ajout api login v1 en get

--- src/Controller/JavaApiController.php
+++ src/Controller/JavaApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MessageRepository;
+use App\Repository\UserRepository;
 use JMS\Serializer\SerializationContext;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,16 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
      */
 class JavaApiController extends AbstractController
 {
-    // /**
-    //  * @Route("/java/api", name="java_api")
-    //  */
-    // public function index(): Response
-    // {
-    //     return $this->render('java_api/index.html.twig', [
-    //         'controller_name' => 'JavaApiController',
-    //     ]);
-    // }
-
+    
     /**
      * retourne la liste des messages du user concerné
      *
@@ -38,6 +30,29 @@ class JavaApiController extends AbstractController
         $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
         return JsonResponse::fromJsonString($serializer->serialize($data, 'json',SerializationContext::create()->enableMaxDepthChecks()));
 
+    }
+
+    /**
+     * permet de tester la connexion à partir de app_stallman
+     *
+     * @Route("/login/{email}/{pwd}", name="api_login",methods={"POST","GET"})
+     * @return void
+     */
+    public function apiLogin(UserRepository $userRepo,$email,$pwd)
+    {
+        //TODO passer cette methode en post parce que là suprême de crassou
+        $user = $userRepo->findOneByEmail(["email"=>$email,"password"=>$pwd]);
+        if(!is_null($user)) {
+            $userData["nom"]=$user->getNom();
+            $userData["prenom"]=$user->getPrenom();
+            $userData["id"]=$user->getId();
+            $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+            return JsonResponse::fromJsonString($serializer->serialize($userData, 'json'));   
+        } else {
+            $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+            return JsonResponse::fromJsonString($serializer->serialize(['user'=> false], 'json'));
+        }
+        
     }
 
 
